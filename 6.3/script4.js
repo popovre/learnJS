808bef6c3052dcd224bee2e0cc64b6906f5d5375
popovre <popov.roman.e@gmail.com>
2023-03-09 12:22:24 +0500

    6.4

--- 6.3/script4.js
+++ 6.3/script4.js
@@ -5,7 +5,7 @@ function makeArmy() {
   while (i < 10) {
     let j = i;
     let shooter = function() { // функция shooter
-      alert( j ); // должна выводить порядковый номер
+      console.log( j ); // должна выводить порядковый номер
     };
     shooters.push(shooter);
     i++;
@@ -16,6 +16,5 @@ function makeArmy() {
 
 let army = makeArmy();
 
-army[0](); // у 0-го стрелка будет номер 10
-army[5](); // и у 5-го стрелка тоже будет номер 10
-// ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3...
+army[0](); 
+army[5](); 
