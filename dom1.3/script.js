48186193c266e4a4da5d9733bf9b9966c44d0f6c
popovre <popov.roman.e@gmail.com>
2023-03-13 13:19:35 +0500

    начинает dom

---
+++ dom1.3/script.js
@@ -0,0 +1,3 @@
+console.log(document.body.firstElementChild);
+console.log(document.body.children[1]);
+console.log(document.body.children[1].lastElementChild);
---
+++ dom1.4/script.js
@@ -0,0 +1,17 @@
+// 1. Таблица с `id="age-table"`.
+let table = document.querySelector('#age-table')
+
+// 2. Все label в этой таблице
+document.querySelectorAll('#age-table label')
+
+// 3. Первый td в этой таблице
+table.querySelector('td')
+
+// 4. Форма с name="search"
+document.querySelector('form[name="search"]')
+
+// 5. Первый input в этой форме
+form.querySelector('input')
+
+// 6. Последний input в этой форме
+let inputs = form.querySelectorAll('input') // найти все input
